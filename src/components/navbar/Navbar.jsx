ae9f707ac40b4feefb6485e458af598ee5fa4025
David Obodo <obododavidsop@gmail.com>
2022-12-02 06:10:25 +0100

    Complete website

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,11 +6,11 @@ import { useState } from 'react'
 
 const Menu = () => (
   <>
-  <p><a href="#home">Home</a></p>
-  <p><a href="#wgpt3"></a>What is GPT?</p>
-  <p><a href="#possibility">Open AI</a></p>
-  <p><a href="#features">Case Studies</a></p>
-  <p><a href="#blog">Library</a></p>
+  <p><a href="#home" aria-label='Link to Home page'>Home</a></p>
+  <p><a href="#wgpt3" aria-label='Link to What is GPT?'>What is GPT?</a></p>
+  <p><a href="#possibility" aria-label='Link to Open AI'>Open AI</a></p>
+  <p><a href="#features" aria-label='Link to Case Studies'>Case Studies</a></p>
+  <p><a href="#blog" aria-label='Link to Library'>Library</a></p>
   </>
 )
 
